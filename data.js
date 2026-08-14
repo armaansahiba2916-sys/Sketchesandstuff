55758e6b75fb12f3f16b0602d691d3d91fdb398d
Sketchesandstuff <armaansahiba2916@gmail.com>
2026-08-14 05:04:01 -0400

    Update alt and hover text for Random Sketch

--- data.js
+++ data.js
@@ -3,8 +3,8 @@ const COMICS = [
     id: 1,
     title: "Random Sketch",
     img: "comics/IMG_20260219_142843.jpg",
-    alt: "Random sketch",
-    hover: "IDK, but Kagami's face looks kinda weird.",
+    alt: "Kagami",
+    hover: "I think I aged him back 5 years.",
     date: "2026-02-15"
   },
 ];
